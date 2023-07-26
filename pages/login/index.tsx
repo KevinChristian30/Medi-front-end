0b5f5e2bf5ea01d46d26ffb4ff9eb95aeec116cf
Kevin Christian <kdotchrist30@gmail.com>
2023-07-26 18:46:28 +0700

    Added OAuth (Sign in with Google)

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,13 +1,36 @@
 import { NextPage } from "next";
 import Navbar from "../../components/navbar/Navbar";
 import style from "./index.module.css";
+import { Button } from "@mui/material";
+import { auth } from "../../firebase/firebaseConfig";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Login: NextPage = () => {
+  const [user, setUser] = useAuthState(auth);
+
+  const googleAuth: GoogleAuthProvider = new GoogleAuthProvider();
+
+  const login = async () => {
+    const result = await signInWithPopup(auth, googleAuth);
+
+
+  };
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return ( 
     <>
       <Navbar />
       <div className={style.loginPageContent}>
-        
+        <Button variant="contained" onClick={() => auth.signOut()}>Log out</Button>
+        <Button variant="contained" onClick={login}>Login</Button>
+        {
+          user && "Welcome, " + user.displayName
+        }
       </div>
     </>
    );
