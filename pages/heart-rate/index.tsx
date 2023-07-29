bf9b7edae642682a23096d5ef3f2749471c5ca92
Kevin Christian <kdotchrist30@gmail.com>
2023-07-29 20:48:30 +0700

    Added Heart Rate Form

--- pages/heart-rate/index.tsx
+++ pages/heart-rate/index.tsx
@@ -1,21 +1,29 @@
 import Head from "next/head";
 import Authorized from "../../components/gates/Authorized";
 import DashboardLayout from "../../layouts/dashboardLayout/dashboardLayout";
+import { LoadingButton } from "@mui/lab";
 
 const HeartRate = () => {
   return (
-    <>
-      <Authorized>
-        <Head>
-          <title>Medi | Heart Rate</title>
-        </Head>
-        <main>
-          <DashboardLayout>
-            <h1>Heart Rate</h1>
-          </DashboardLayout>
-        </main>
-      </Authorized>
-    </>
+    <Authorized>
+      <Head>
+        <title>Medi | Heart Rate</title>
+      </Head>
+      <main>
+        <DashboardLayout>
+          <h1>Heart Rate</h1>
+          <br />
+          <p>Using the power of AI, we can get your heart rate with a 15 second video of your forehead.</p>
+          <br />
+          <input type="file" />
+          <LoadingButton
+            variant="outlined"
+          >
+            Submit
+          </LoadingButton>
+        </DashboardLayout>
+      </main>
+    </Authorized>
   )
 }
 
