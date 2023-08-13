3e3bf11ecba82a59d9a9d9f89425f9ac585ea36a
Kevin Christian <kdotchrist30@gmail.com>
2023-08-13 12:19:39 +0700

    Added MediPlus Brain Tumor Classifier (Backend Problem)

--- pages/medi-plus/brain-tumor-classifier/index.tsx
+++ pages/medi-plus/brain-tumor-classifier/index.tsx
@@ -5,9 +5,11 @@ import { useState } from "react";
 import useRequest from '../../../hooks/useRequest';
 import Response from '../../../models/utility/Response';
 import Error from '../../../types/error';
+import Image from 'next/image';
 
 interface FormValues {
-  image: any;
+  image: File | null;
+  imagePreviewURL: string | null;
 }
 
 interface FormErrors {
@@ -16,6 +18,16 @@ interface FormErrors {
 
 const BrainTumorClassifier = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState<FormValues>({
+    image: null,
+    imagePreviewURL: null
+  });
+  const [formErrors, setFormErrors] = useState<FormErrors>({
+    image: {
+      isError: false,
+      message: ''
+    }
+  });
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,9 +35,27 @@ const BrainTumorClassifier = () => {
     const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
     const response: Response<string> = await useRequest({
-      url: backendURL,
-      method: 'POST'
+      url: `${backendURL}/medi-plus/brain-tumor-classifier`,
+      method: 'POST',
+      data: formValues
     });
+    
+    console.log(response);
+  }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      setFormValues({...formValues, image: files[0]});
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFormValues({...formValues, imagePreviewURL: reader.result as string});
+      };
+
+      reader.readAsDataURL(files[0]);
+    }
   }
   
   return (
@@ -34,7 +64,7 @@ const BrainTumorClassifier = () => {
         title='Brain Tumor Classifier'
       >
         <form onSubmit={handleFormSubmit}>
-          <input type="file" />
+          <input type="file" onChange={handleImageChange} />
           <LoadingButton 
             type="submit"
             variant="outlined" 
@@ -42,6 +72,15 @@ const BrainTumorClassifier = () => {
             >
             Classify
           </LoadingButton>
+          {
+            formValues.imagePreviewURL &&
+            <Image
+              src={formValues.imagePreviewURL}
+              alt='Brain Tumor'
+              width={200}
+              height={100}
+            />
+          }
         </form>
       </MediplusLayout>
     </div>
