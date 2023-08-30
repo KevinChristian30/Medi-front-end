93c092b5ceb053e3525a8cf6bda2732f012eb82e
Kevin Christian <kdotchrist30@gmail.com>
2023-08-30 11:55:59 +0700

    Validated Form Values

--- components/forms/sleepAnalysisForm/sleepAnalysisForm.tsx
+++ components/forms/sleepAnalysisForm/sleepAnalysisForm.tsx
@@ -14,12 +14,12 @@ interface FormValues {
   gender: string;
   age: number;
   BMICategory: string;
-  sleepDuration?: number;
-  qualityOfSleep?: number;
+  sleepDuration?: string;
+  qualityOfSleep?: string;
   physicalActivity?: number;
-  stressLevel?: number;
-  upperBloodPressure?: number;
-  lowerBloodPressure?: number;
+  stressLevel?: string;
+  upperBloodPressure?: string;
+  lowerBloodPressure?: string;
 }
 
 interface FormErrors {
@@ -38,12 +38,12 @@ const SleepAnalysisForm = () => {
     gender: '',
     age: 0,
     BMICategory: '',
-    sleepDuration: undefined,
-    qualityOfSleep: undefined,
-    physicalActivity: undefined,
-    stressLevel: undefined,
-    lowerBloodPressure: undefined,
-    upperBloodPressure: undefined,
+    sleepDuration: '',
+    qualityOfSleep: '',
+    physicalActivity: 0,
+    stressLevel: '',
+    lowerBloodPressure: '',
+    upperBloodPressure: '',
   });
   const [formErrors, setFormErrors] = useState<FormErrors>({
     sleepDuration: {
@@ -80,12 +80,12 @@ const SleepAnalysisForm = () => {
         gender: '',
         age: 0,
         BMICategory: '',
-        sleepDuration: undefined,
-        qualityOfSleep: undefined,
-        physicalActivity: undefined,
-        stressLevel: undefined,
-        lowerBloodPressure: undefined,
-        upperBloodPressure: undefined,
+        sleepDuration: '',
+        qualityOfSleep: '',
+        physicalActivity: 0,
+        stressLevel: '',
+        lowerBloodPressure: '',
+        upperBloodPressure: '',
       };
 
       if (!user) {
@@ -111,10 +111,125 @@ const SleepAnalysisForm = () => {
     fetch();
   }, []);
 
+  const isFormValid = () : boolean => {
+    let localFormErrors: FormErrors = {
+      sleepDuration: {
+        isError: false,
+        message: ''
+      },
+      qualityOfSleep: {
+        isError: false,
+        message: ''
+      },
+      physicalActivity: {
+        isError: false,
+        message: ''
+      },
+      stressLevel: {
+        isError: false,
+        message: ''
+      },
+      upperBloodPressure: {
+        isError: false,
+        message: ''
+      },
+      lowerBloodPressure: {
+        isError: false,
+        message: ''
+      },
+    };
+
+    const validateSleepDuration = () => {
+      if (Number(formValues.sleepDuration) > 0) return true;
+
+      localFormErrors.sleepDuration = {
+        isError: true,
+        message: 'Sleep duration must be greater than 0.'
+      };
+
+      return false;
+    }
+
+    const validateQualityOfSleep = () => {
+      if (Number(formValues.qualityOfSleep) >= 1 && Number(formValues.qualityOfSleep) <= 10) return true;
+
+      localFormErrors.qualityOfSleep = {
+        isError: true,
+        message: 'Quality of sleep must be greater between 1 and 10.'
+      };
+
+      return false;
+    }
+
+    const validatePhysicalActivity = () => {
+      if (Number(formValues.physicalActivity) > -1) return true;
+
+      localFormErrors.physicalActivity = {
+        isError: true,
+        message: 'Minutes of physical activity musn\'t be negative.'
+      };
+
+      return false;
+    }
+
+    const validateStressLevel = () => {
+      if (Number(formValues.stressLevel) >= 1 && Number(formValues.stressLevel) <= 10) return true;
+
+      localFormErrors.stressLevel = {
+        isError: true,
+        message: 'Stress level must be greater between 1 and 10.'
+      };
+
+      return false;
+    }
+
+    const validateLowerBloodPressure = () => {
+      if (Number(formValues.lowerBloodPressure) > 0) return true;
+
+      localFormErrors.lowerBloodPressure = {
+        isError: true,
+        message: 'Lower blood pressure must be greater than 0'
+      };
+
+      return false;
+    }
+
+    const validateUpperBloodPressure = () => {
+      if (Number(formValues.upperBloodPressure) > 0) return true;
+
+      localFormErrors.upperBloodPressure = {
+        isError: true,
+        message: 'Upper blood pressure must be greater than 0'
+      };
+
+      return false;
+    }
+    
+    const isValidSleepDuration = validateSleepDuration();
+    const isValidQualityOfSleep = validateQualityOfSleep();
+    const isValidPhysicalActivity = validatePhysicalActivity();
+    const isValidStressLevel = validateStressLevel();
+    const isValidLowerBloodPressure = validateLowerBloodPressure();
+    const isValidUpperBloodPressure = validateUpperBloodPressure();
+    
+    setFormErrors(localFormErrors);
+
+    return isValidSleepDuration &&
+           isValidQualityOfSleep &&
+           isValidPhysicalActivity &&
+           isValidStressLevel &&
+           isValidLowerBloodPressure &&
+           isValidUpperBloodPressure;
+  }
+
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    
+    if (!isFormValid()) return;
+
+    console.log(formValues);
+
+    // Todo: Send Data to Backend
   }
 
   return (
@@ -167,7 +282,7 @@ const SleepAnalysisForm = () => {
                 placeholder='Average Sleep Duration'
                 fullWidth
                 value={formValues.sleepDuration}
-                onChange={(e) => setFormValues({...formValues, sleepDuration: Number(e.target.value)})}
+                onChange={(e) => setFormValues({...formValues, sleepDuration: e.target.value})}
                 error={formErrors.sleepDuration.isError}
                 helperText={formErrors.sleepDuration.message}
                 size='small'
@@ -186,7 +301,7 @@ const SleepAnalysisForm = () => {
                 placeholder='Your subjective sleep quality on a scale of 1 - 10'
                 fullWidth
                 value={formValues.qualityOfSleep}
-                onChange={(e) => setFormValues({...formValues, qualityOfSleep: Number(e.target.value)})}
+                onChange={(e) => setFormValues({...formValues, qualityOfSleep: e.target.value})}
                 error={formErrors.qualityOfSleep.isError}
                 helperText={formErrors.qualityOfSleep.message}
                 size='small'
@@ -200,7 +315,7 @@ const SleepAnalysisForm = () => {
                 placeholder='Your subjective stress level on a scale of 1 - 10'
                 fullWidth
                 value={formValues.stressLevel}
-                onChange={(e) => setFormValues({...formValues, stressLevel: Number(e.target.value)})}
+                onChange={(e) => setFormValues({...formValues, stressLevel: e.target.value})}
                 error={formErrors.stressLevel.isError}
                 helperText={formErrors.stressLevel.message}
                 size='small'
@@ -228,11 +343,11 @@ const SleepAnalysisForm = () => {
             <div className={style.line}>
               <TextField 
                 type='number'
-                label='Blood Pressure (Upper/Systolic)'
+                label='Blood Pressure (Lower/Diastolic)'
                 placeholder='Your systolic blood pressure'
                 fullWidth
                 value={formValues.lowerBloodPressure}
-                onChange={(e) => setFormValues({...formValues, lowerBloodPressure: Number(e.target.value)})}
+                onChange={(e) => setFormValues({...formValues, lowerBloodPressure: e.target.value})}
                 error={formErrors.lowerBloodPressure.isError}
                 helperText={formErrors.lowerBloodPressure.message}
                 size='small'
@@ -244,11 +359,11 @@ const SleepAnalysisForm = () => {
             <div className={style.line}>
               <TextField 
                 type='number'
-                label='Blood Pressure (Upper/Diastolic)'
+                label='Blood Pressure (Upper/Systolic)'
                 placeholder='Your diastolic blood pressure'
                 fullWidth
                 value={formValues.upperBloodPressure}
-                onChange={(e) => setFormValues({...formValues, upperBloodPressure: Number(e.target.value)})}
+                onChange={(e) => setFormValues({...formValues, upperBloodPressure: e.target.value})}
                 error={formErrors.upperBloodPressure.isError}
                 helperText={formErrors.upperBloodPressure.message}
                 size='small'
@@ -261,6 +376,7 @@ const SleepAnalysisForm = () => {
               <Button 
                 variant="contained"
                 fullWidth
+                type='submit'
               >
                 Analyze My Sleep
               </Button>
